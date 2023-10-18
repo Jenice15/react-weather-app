9f4bea177c1542a47256b979882f7d12a7aaf3e1
Jenice15 <jenicemartin15@gmail.com>
2023-10-17 23:14:02 -0700

    created weatherforecast component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Search from "./Search";
+import WeatherForecast from "./WeatherForecast";
+import WeatherSummary from "./WeatherSummary";
 
 function App() {
     return (
         <div className="App">
             <div className="Wrapper">
                 <h1 className="header">Weather App</h1>
+                <Search />
+                <WeatherSummary />
+                <WeatherForecast />
             </div>
-            <Search />
         </div>
     );
 }
---
+++ src/WeatherForecast.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./App.css";
+import ReactAnimatedWeather from "react-animated-weather";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function WeatherForecast() {
+    return (
+        <div className="weather-forecast">
+            <div className="container">
+                <div class="weather-table ">
+                    <div className="row-1">
+                        <div className="col-md-3">
+                            <div className="forecast-day">Wed</div>
+                            <div className="forecast-icon">
+                                <ReactAnimatedWeather
+                                    className="animated-icon"
+                                    icon="CLEAR_DAY"
+                                    color="orange"
+                                    size="60"
+                                    animate="true"
+                                />
+                            </div>
+                            <div className="forecast-temp">20&deg;</div>
+                        </div>
+                    </div>
+                    <div className="row-2">
+                        <div className="col-md-3">
+                            <div className="forecast-day">Thu</div>
+                            <div className="forecast-icon">
+                                <ReactAnimatedWeather
+                                    className="animated-icon"
+                                    icon="RAIN"
+                                    color="Blue"
+                                    size="60"
+                                    animate="true"
+                                />
+                            </div>
+                            <div className="forecast-temp">15&deg;</div>
+                        </div>
+                    </div>
+                    <div className="row-3">
+                        <div className="col-md-3">
+                            <div className="forecast-day">Fri</div>
+                            <div className="forecast-icon">
+                                <ReactAnimatedWeather
+                                    className="animated-icon"
+                                    icon="CLOUDY"
+                                    color="blue"
+                                    size="60"
+                                    animate="true"
+                                />
+                            </div>
+                            <div className="forecast-temp">18&deg;</div>
+                        </div>
+                    </div>
+
+                    <div className="row-4">
+                        <div className="col-md-3">
+                            <div className="forecast-day">Sat</div>
+                            <div className="forecast-icon">
+                                <ReactAnimatedWeather
+                                    className="animated-icon"
+                                    icon="CLOUDY"
+                                    color="blue"
+                                    size="60"
+                                    animate="true"
+                                />
+                            </div>
+                            <div className="forecast-temp">17&deg;</div>
+                        </div>
+                    </div>
+                    <div className="row-4">
+                        <div className="col-md-3">
+                            <div className="forecast-day">Sun</div>
+                            <div className="forecast-icon">
+                                <ReactAnimatedWeather
+                                    className="animated-icon"
+                                    icon="WIND"
+                                    color="grey"
+                                    size="60"
+                                    animate="true"
+                                />
+                            </div>
+                            <div className="forecast-temp">20&deg;</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
